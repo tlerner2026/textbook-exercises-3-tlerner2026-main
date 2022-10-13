8349797c91bd3df5c37a493bfc78ed86e7dbbf26
Tal Lerner <tlerner2026@kehillahstudent.org>
2022-10-13 11:31:17 -0700

    sum_100_evens

--- q_2_8.py
+++ q_2_8.py
@@ -8,9 +8,14 @@ Python Programming in Context
 
 sum_100_evens = 0
 # USE A FOR LOOP TO UPDATE THE VALUE OF sum_100_evens
-
+"""
+The loop starts at 0 and has a step of 2 to 101 therefore adding all numbers
+0-100 that are even. The step by 2 ensures they are even and counting to 101
+ensures that 100 is also counted. 
+"""
 for num in range(0,101,2):
     sum_100_evens=num+sum_100_evens
+
 
 
 # This is an f string. It starts with `f` before the quotation marks. We can use
